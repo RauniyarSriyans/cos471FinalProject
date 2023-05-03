2fc36a98c7f1477c28cffa0fd21e0f428f140a66
Karim Elbarbary <karime@princeton.edu>
2023-05-03 07:45:33 -0400

    Clear Git cache

--- give-forever/webpack.config.js
+++
@@ -1,8 +0,0 @@
-resolve: {
-    fallback: {
-      "os": require.resolve("os-browserify/browser"),
-      "https": require.resolve("https-browserify"),
-      "http": require.resolve("stream-http"),
-      "crypto": require.resolve("crypto-browserify")
-    }
-  }
