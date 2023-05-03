51f6760dabd96c5d7d2e382501724b12af4310b0
Karim Elbarbary <karime@princeton.edu>
2023-05-03 07:25:28 -0400

    connecting app to local blockchain and changing webpack configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Web3 from "web3"
+import Election from './contracts/Election.json';
+import {Route, Routes, Router} from 'react-router-dom';
+import LandingPage from "./LandingPage"
 
-function App() {
+
+function App(): React.ReactNode {
+  const [state, setState] = useState({web3:null, contract:null});
+  useEffect(() => {
+    const provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545");
+    async function template(){
+      const web3 = new Web3(provider);
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = Election.networks[networkId];
+      const contract = new web3.eth.Contract(Election.abi, deployedNetwork.address);
+      setState({web3:web3, contract: contract});
+
+
+    }
+    provider && template();
+  }, []);
+
+  useEffect(()=> {
+    const {contract} = state;
+    async function test(){
+      const data = await contract.methods.sayHello().call();
+      console.log(data);
+    }
+    contract && test();
+  }, [state]);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+   <div>
+      <Routes>
+      <Route exact path ="/" element={<LandingPage/>}/>
+      
+    </Routes>
+   </div>
   );
 }
+
 
 export default App;
