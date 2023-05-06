a6cdfee77a59620e27751f1cbd0d8de40d24e919
Karim Elbarbary <karime@princeton.edu>
2023-05-06 00:51:30 -0400

    work

---
+++ src/LandingPage.js
@@ -0,0 +1,72 @@
+import './LandingPage.css';
+import { useState, useEffect } from "react";
+import Web3 from "web3"
+import Election from './contracts/Election.json';
+
+
+export default function LandingPage(): React.ReactNode {
+  const [state, setState] = useState({web3:null, contract:null});
+  const [isConnected, setIsConnected] = useState(false);
+  const handleConnect = () => {
+        /*
+    if (window.ethereum) {
+      provider = window.ethereum;
+      console.log("1");
+    } else if (window.web3){
+      provider = window.web3.currentProvider;
+      console.log("2");
+    }
+    else{*/
+    const provider = new Web3.providers.HttpProvider("HTTP://127.0.0.1:7545");
+  //}
+    async function template(){
+      const web3 = new Web3(provider);
+      const networkId = await web3.eth.net.getId();
+      const deployedNetwork = Election.networks[networkId];
+      const contract = new web3.eth.Contract(Election.abi, deployedNetwork.address);
+      setState({web3:web3, contract: contract});
+      setIsConnected(true);
+    } 
+    provider && template();
+  }
+
+  useEffect(()=> {
+    const {contract} = state;
+    async function test(){
+      const data = await contract.methods.sayHello().call();
+      console.log(data);
+    }
+    contract && test();
+  }, [state.contract]);
+
+  
+
+  return (
+    <div>
+      <header className="header">
+        <div className="left-section">
+          <h2>DeVote</h2>
+        </div>
+        <div className='right-section'>
+            <h2>A COS471 Project</h2>
+        </div>
+      </header>
+      <div class="main-text">
+  <p>Welcome to</p>
+</div>
+      <div class="center-text">
+  <p>DeVote</p>
+</div>
+<div>
+    <h1>Button</h1>
+    {isConnected ? (
+      <p>Placeholder for token</p>
+    ) : (
+      <button onClick={handleConnect}>Connect</button>
+    )}
+  </div>
+
+
+    </div>
+  );
+};
