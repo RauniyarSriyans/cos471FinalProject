b0ea8a44de5bfd88d6eec564f213dd9e64bd4892
Karim Elbarbary <karime@princeton.edu>
2023-05-09 00:48:49 -0400

    election results page

--- src/App.js
+++ src/App.js
@@ -4,11 +4,15 @@ import LandingPage from "./LandingPage";
 import VoterDashboard from "./VoterDashboard";
 import Login from "./Login"
 import "bootstrap/dist/css/bootstrap.min.css";
+import ElectionResults from "./ElectionResults";
+
+
 
 function App() {
   return (
     <Routes>
       <Route exact path="/dashboard" element={<VoterDashboard />} />
+      <Route exact path="/ElectionResults" element={<ElectionResults />} />
       <Route exact path="/login" element={<Login />} />
       <Route exact path="/" element={<LandingPage />} />
     </Routes>
